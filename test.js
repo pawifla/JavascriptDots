c29b48a4ed27dc41e1deb3a903c281897a291c07
pawifla <willflanagan@gmail.com>
2023-08-29 05:27:36 -0700

    add health icons

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const circle = document.getElementById('circle');
-console.log(circle);
 const container = document.getElementById('container');
+const health = document.getElementById('health');
 const corners = [
     { top: 0, left: 0 },
     { top: 0, right: 0 },
@@ -8,6 +8,7 @@ const corners = [
     { bottom: 0, left: 0 }
 ];
 
+let livesRemaining = 3;
 let currentCorner = 0;
 let animationInProgress = false;
 
@@ -15,12 +16,9 @@ let animationInProgress = false;
 let direction = '';
 let options = [];
 function animateCircle() {
-    let count = 0;
     let pos = 0;
     let id = null;
     let startPos = { x: parseInt(circle.style.left.replace(/\D/g, '')), y: parseInt(circle.style.top.replace(/\D/g, '')) };
-    circle.style.top = startPos.y;
-    circle.style.left = startPos.x;
 
     // pick random direction based on start positions
     direction = pickDirections(startPos);
@@ -28,60 +26,43 @@ function animateCircle() {
     clearInterval(id);
     id = setInterval(frame, 1);
     function frame() {
-        if (count == 350) {
+        if (pos == 350) {
             clearInterval(id);
         } else {
             switch (direction) {
                 case 'down':
-                    // code block
                     pos++;
-                    count++;
                     moveCircleDown(pos);
                     break;
                 case 'up':
-                    // code block
                     pos++;
-                    count++;
                     moveCircleUp(pos);
                     break;
                 case 'right':
-                    // code block
                     pos++;
-                    count++;
                     moveCircleRight(pos);
                     break;
                 case 'left':
-                    // code block
                     pos++;
-                    count++;
                     moveCircleLeft(pos);
                     break;
                 case 'downRight':
-                    // code block
                     pos++;
-                    count++;
                     moveCircleDownRight(pos);
                     break;
                 case 'downLeft':
-                    // code block
                     pos++;
-                    count++;
                     moveCircleDownLeft(pos);
                     break;
                 case 'upRight':
-                    // code block
                     pos++;
-                    count++;
                     moveCircleUpRight(pos);
                     break;
                 case 'upLeft':
-                    // code block
                     pos++;
-                    count++;
                     moveCircleUpLeft(pos);
                     break;
                 default:
-                // code block
             }
         }
         //4 corners
@@ -141,9 +122,29 @@ function pickDirections(startPos) {
     return direction;
 }
 
+function loseLife(){
+    const healthContainer = document.getElementById('health');
+    console.log(healthContainer.getElementsByClassName('bi'));
+    healthContainer.removeChild(healthContainer.getElementsByClassName('bi')[0]);
+}
+
+function createLives(numOfLives){
+    const iconElem = document.createElement('i');
+    iconElem.setAttribute('class','bi bi-heart-fill');
+    iconElem.setAttribute('id', numOfLives);
+    health.appendChild(iconElem);
+}
+
 // Initial position
 circle.style.left = '0';
 circle.style.top = '0';
 
+//Load 3 lives
+for(numOfLives = 3; numOfLives > 0; numOfLives--){
+    createLives(numOfLives);
+}
+
+//Start Timer
+
 // Move to the next corner on click
-container.addEventListener('click', animateCircle);
+container.addEventListener('click', loseLife);
